3034c9f682336902b0dac26b8aad0e5f0cccc04b
Saurabh <saurabh.mehare@gmail.com>
2022-12-24 18:33:10 +0530

    complete accordion js project

--- JavaScript Projects/01Accordion/faq.js
+++ JavaScript Projects/01Accordion/faq.js
@@ -18,19 +18,85 @@ const faqData = [
   },
 ];
 
-const accordianBody;
+const accordianBody = document.querySelector(".accordian_body");
+
 const faqs = [];
+for(let i=0; i<faqData.length;i++){
+  let elem = faqData[i];
+  faqs.push(elem);
+}
+
+
+
+function createFaq() {
+
+  let creatMain = document.createElement("main");
+  accordianBody.append(creatMain);
+  creatMain.setAttribute("class","faq faq_header");
+  
+  let creatHeader = document.createElement("header");
+  creatMain.appendChild(creatHeader);
+  creatHeader.setAttribute("class","faq_header");
+
+  let creatH = document.createElement("h3");
+  creatHeader.appendChild(creatH);
+  creatH.setAttribute("class", "faq ");
+
+  
+  let creatBtn = document.createElement("button");
+  creatHeader.appendChild(creatBtn);
+  creatBtn.setAttribute("class", " show_btn")
+  creatBtn.textContent="+";
+  
+  let creatP = document.createElement("p");
+  creatMain.appendChild(creatP);
+  creatP.setAttribute("class", "faq hidden");
+
+}
+
+for(let i=0; i<faqs.length; i++){
+  createFaq();
+
+}  
+
+
 
 function showFaq() {
   
-}
+  for(let i=0; i<faqs.length; i++){
 
-function createFaq() {
+    document.querySelectorAll("h3")[i].innerHTML=faqs[i].question
+    document.querySelectorAll("p")[i].innerHTML=faqs[i].answer
+    
+  }
+
+  }
   
-}
+  showFaq()
+
+  
 
 function btnStatusUpdate() {
+
+  for(let i=0; i<faqs.length; i++){
+    let para = document.querySelectorAll(".hidden").item(i);
+
+
+    let btn = document.querySelectorAll("button");
   
+    btn.item(i).addEventListener("click", hidenFun);
+    function hidenFun (){
+      if (para.style.display !== "none") {
+        para.style.display = "none";
+      } else {
+        para.style.display = "block";
+      }
+    }
+
+  }
+
 }
+
+btnStatusUpdate()
 
 
